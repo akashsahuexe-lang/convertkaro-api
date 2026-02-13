c380e118de124a9e033ce16c8bf621512a99a344
akashsahuexe-lang <akashsahu.exe@gmail.com>
2026-02-13 20:48:40 +0530

    Update pdfSplit.routes.js

--- src/routes/pdfSplit.routes.js
+++ src/routes/pdfSplit.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const { splitPdf } = require("../controllers/pdfSplit.controller");
 
-const upload = multer({ dest: "/tmp/" });
+const upload = multer({ dest: "/tmp/", });
 const router = express.Router();
 
 router.post("/", upload.single("pdf"), splitPdf);
