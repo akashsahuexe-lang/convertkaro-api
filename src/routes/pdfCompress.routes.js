b45d4e18cc2913baaf30efb3e6f90b1eafed65f6
akashsahuexe-lang <akashsahu.exe@gmail.com>
2026-02-13 20:48:26 +0530

    Update pdfCompress.routes.js

--- src/routes/pdfCompress.routes.js
+++ src/routes/pdfCompress.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const multer = require("multer");
 const { compressPdf } = require("../controllers/pdfCompress.controller");
 
-const upload = multer({dest: "/tmp/" });
+const upload = multer({dest: "/tmp/", });
 const router = express.Router();
 
 router.post("/", upload.single("pdf"), compressPdf);
