0df24dd040f923950f165f2b5df481996e159508
akashsahuexe-lang <akashsahu.exe@gmail.com>
2026-02-14 10:10:59 +0530

    Update pdfMerge.controller.js

--- src/controllers/pdfMerge.controller.js
+++ src/controllers/pdfMerge.controller.js
@@ -7,14 +7,14 @@ exports.mergePdf = async (req, res) => {
       return res.status(400).json({ error: "Minimum 2 PDFs required" });
     }
 
-   const mergedPdfBytes = await mergePdfService(req.files);
+    const mergedPdfBytes = await mergePdfService(req.files);
 
-res.setHeader("Content-Type", "application/pdf");
-res.setHeader("Content-Disposition", "attachment; filename=merged.pdf");
-res.send(mergedPdfBytes);
+    res.setHeader("Content-Type", "application/pdf");
+    res.setHeader("Content-Disposition", "attachment; filename=merged.pdf");
+    res.send(mergedPdfBytes);
 
   } catch (error) {
-    console.error(error);
+    console.error("Merge Controller Error:", error);
     res.status(500).json({ error: "PDF merge failed" });
   }
 };
